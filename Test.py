84f6209b83c4d0393998bfc602e1edcfdb892e7b
Stefan <stefan@layereight.de>
2019-12-05 11:39:15 +0100

    refactored test

--- Test.py
+++ Test.py
@@ -7,38 +7,22 @@ from Cli import fizzbuzz
 
 class SimpleTestCase(unittest.TestCase):
 
-    def test_one(self):
-        result = fizzbuzz(1)
-        self.assertEqual("1", result)
+    testCases = [
+        [1, "1"],
+        [2, "2"],
+        [3, "Fizz"],
+        [4, "4"],
+        [5, "Buzz"],
+        [6, "Fizz"],
+        [7, "7"],
+        [8, "8"],
+        [9, "Fizz"],
+    ]
 
-    def test_two(self):
-        result = fizzbuzz(2)
-        self.assertEqual("2", result)
-
-    def test_three(self):
-        result = fizzbuzz(3)
-        self.assertEqual("Fizz", result)
-
-    def test_four(self):
-        result = fizzbuzz(4)
-        self.assertEqual("4", result)
-
-    def test_five(self):
-        result = fizzbuzz(5)
-        self.assertEqual("Buzz", result)
-
-    def test_six(self):
-        result = fizzbuzz(6)
-        self.assertEqual("Fizz", result)
-
-    def test_nine(self):
-        result = fizzbuzz(9)
-        self.assertEqual("Fizz", result)
-
-
-
-
-
+    def test_TestCases(self):
+        for testCase in self.testCases:
+            result = fizzbuzz(testCase[0])
+            self.assertEqual(testCase[1], result)
 
 
 if "__main__" == __name__:
