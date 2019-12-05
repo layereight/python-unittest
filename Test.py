73904216a9f54d695236b517746893f8b1fb7615
Stefan <stefan@layereight.de>
2019-12-05 11:47:07 +0100

    fails for 30

--- Test.py
+++ Test.py
@@ -23,6 +23,21 @@ class SimpleTestCase(unittest.TestCase):
         [13, "13"],
         [14, "14"],
         [15, "FizzBuzz"],
+        [16, "16"],
+        [17, "17"],
+        [18, "Fizz"],
+        [19, "19"],
+        [20, "Buzz"],
+        [21, "Fizz"],
+        [22, "22"],
+        [23, "23"],
+        [24, "Fizz"],
+        [25, "Buzz"],
+        [26, "26"],
+        [27, "Fizz"],
+        [28, "28"],
+        [29, "29"],
+        [30, "FizzBuzz"],
     ]
 
     def test_TestCases(self):
